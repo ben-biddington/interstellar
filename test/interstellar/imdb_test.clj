a550d0d3fa43c7aa84d8f05360b7be01e759614f
Ben Biddington <ben.biddington@gmail.com>
2015-01-26 21:42:59 +1300

    [=> GREEN] Can find RoboCop and it has score 67

--- test/interstellar/imdb_test.clj
+++ test/interstellar/imdb_test.clj
@@ -7,12 +7,15 @@
 (defn imdb-find [name]
 
 (let [{:keys [status headers body error]} @(http/get "http://www.omdbapi.com/?t=robocop")]
-	(println headers)
-	(println status)
-	(println body)
-	(println (get body "Metascore"))
-	{:name name}))
+	;(println headers)
+	;(println status)
+	;(println body)
+	(let [jsontext (json/read-str body :key-fn keyword)] 
+{:name (get jsontext :Title) :score (get jsontext :Metascore)}
+) 
+	)
+)
 
 (deftest a-test
   (testing "can find robocop"
-    (is (= {:name "robocop"} (imdb-find "robocop")))))
+    (is (= {:name "RoboCop" :score "67"} (imdb-find "robocop")))))
