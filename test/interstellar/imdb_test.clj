1e67934c882b7a0ae4012b2c9bfc2dd1e6a022bb
Ben Biddington <ben.biddington@gmail.com>
2015-01-26 21:56:57 +1300

    Actually use the name supplied

--- test/interstellar/imdb_test.clj
+++ test/interstellar/imdb_test.clj
@@ -4,18 +4,15 @@
 	    [org.httpkit.client :as http]
 	    [clojure.data.json :as json]
 	    ))
-(defn imdb-find [name]
 
-(let [{:keys [status headers body error]} @(http/get "http://www.omdbapi.com/?t=robocop")]
-	;(println headers)
-	;(println status)
-	;(println body)
-	(let [jsontext (json/read-str body :key-fn keyword)] 
-{:name (get jsontext :Title) :score (get jsontext :Metascore)}
-) 
-	)
-)
+(defn imdb-find [name]
+	(let [{:keys [status headers body error]} @(http/get "http://www.omdbapi.com" {:query-params {:t name}})]
+		(let [jsontext (json/read-str body :key-fn keyword)] 
+		{
+			:name  (get jsontext :Title) 
+			:score (Integer/parseInt(get jsontext :Metascore))
+		})))
 
 (deftest a-test
   (testing "can find robocop"
-    (is (= {:name "RoboCop" :score "67"} (imdb-find "robocop")))))
+    (is (= {:name "RoboCop" :score 67} (imdb-find "robocop")))))
