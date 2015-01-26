5d6698985648ac2509c7dbaab6c1ab355fe74708
Ben Biddington <ben.biddington@gmail.com>
2015-01-26 20:47:46 +1300

    Finally imported <http-kit>...

--- test/interstellar/imdb_test.clj
+++ test/interstellar/imdb_test.clj
@@ -1,10 +1,12 @@
 (ns interstellar.imdb-test
   (:require [clojure.test :refer :all]
-            [interstellar.core :refer :all]))
-
+            [interstellar.core :refer :all]
+	    [org.httpkit.client :as http]
+	    ))
 (defn imdb-find [name]
-	{:name name}
-	)
+(let reply  
+	(http/get "http://www.omdbapi.com/?t=robocop"))
+	{:name name})
 
 (deftest a-test
   (testing "can find robocop"
