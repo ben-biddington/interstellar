4794137c1cfa18977f9c4bfd23db42b3b643144e
Tim Biddington <tim.biddington@gmail.com>
2015-01-30 09:08:26 +1100

    Updated proj description and 'earl'

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
 (defproject interstellar "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An web client for selecting only worthwhile fillums"
+  :url "www.github.com/ben-biddington/interstellar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"] [http-kit "2.1.16"] [org.clojure/data.json "0.2.5"]]
+  :dependencies [
+  		[org.clojure/clojure "1.6.0"]
+  		[http-kit "2.1.16"]
+  		[org.clojure/data.json "0.2.5"]]
   :main ^:skip-aot interstellar.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
