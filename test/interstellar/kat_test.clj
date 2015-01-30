ab10c525ef703cb8537d8955c3c005c9de3b0434
Ben Biddington <ben.biddington@gmail.com>
2015-01-30 19:43:16 +1300

    Formatting

--- test/interstellar/kat_test.clj
+++ test/interstellar/kat_test.clj
@@ -22,23 +22,17 @@
   (testing "Make a web request and select the title like this"
     (let [result (title "https://www.google.com")]
       (is (= '("Google") (get result :content)))
-
       ))
 
   (testing "Select all links like this"
     (let [result (links "https://www.google.com")]
       (is (< 0 (count result)))
-
       ))
 
   (testing "Select all links with css class by filtering like this"
     (let [result (filter (fn [e] (has-class? e "gb1")) (links "https://www.google.com"))]
       (println result)
       (is (< 0 (count result)))
-
       ))
-
-
-
 
 	)
