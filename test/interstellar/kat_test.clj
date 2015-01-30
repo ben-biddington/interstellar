2212078e7a047159f2edfb06ca3faf47fd5056b5
Ben Biddington <ben.biddington@gmail.com>
2015-01-30 15:51:47 +1300

    Select all links like this

--- test/interstellar/kat_test.clj
+++ test/interstellar/kat_test.clj
@@ -9,11 +9,24 @@
 		(select [:title])
 	)))
 
+(defn links[earl]
+	(-> earl URL. html-resource
+		(select [:a])
+	))
+
 (deftest reading-web-pages
   (testing "Make a web request and select the title like this"
     (let [result (title "https://www.google.com")]
       (is (= '("Google") (get result :content)))
 
       ))
+
+  (testing "Select all links like this"
+    (let [result (links "https://www.google.com")]
+      (is (< 0 (count result)))
+
+      ))
+
+
 
 	)
