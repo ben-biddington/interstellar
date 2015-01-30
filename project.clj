110ed31ba98f1c36ed5d7d35e8a49cf28826ff94
Ben Biddington <ben.biddington@gmail.com>
2015-01-30 15:16:04 +1300

    Added <enlive>

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
   :dependencies [
   		[org.clojure/clojure "1.6.0"]
   		[http-kit "2.1.16"]
-  		[org.clojure/data.json "0.2.5"]]
+  		[org.clojure/data.json "0.2.5"]
+  		[enlive "1.1.5"]]
   :main ^:skip-aot interstellar.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
