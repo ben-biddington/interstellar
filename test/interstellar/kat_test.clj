01f1e4536398d38ef2d2cfff23bc4ece421b9323
Ben Biddington <ben.biddington@gmail.com>
2015-01-31 11:24:39 +1300

    Understanding ->

--- test/interstellar/kat_test.clj
+++ test/interstellar/kat_test.clj
@@ -2,12 +2,20 @@
   (:use net.cgrand.enlive-html)
   (:import java.net.URL) 
   (:require [clojure.test :refer :all]
-            [interstellar.core :refer :all])) 
+            [interstellar.core :refer :all]))
 
 (defn title[earl]
 	(first (-> earl URL. html-resource
 		(select [:title])
 	)))
+
+(defn to-earl[text]
+	(URL. text)
+)
+
+(defn body[earl]
+	(select (html-resource (to-earl earl)) [:body])
+	)
 
 (defn links[earl]
 	(-> earl URL. html-resource
@@ -15,23 +23,34 @@
 	))
 
 (defn has-class?[element, name]
-	(= name (get (get element :attrs) :class))
-	)
+	(= name (get (get element :attrs) :class)))
+
+(def earl "https://www.google.com")
 
 (deftest reading-web-pages
+
+  (testing "Make a web request and select the body like this"
+    (let [result (body earl)]
+      (is (< 0 (count result)))
+      ))
+
   (testing "Make a web request and select the title like this"
-    (let [result (title "https://www.google.com")]
+    (let [result (title earl)]
       (is (= '("Google") (get result :content)))
       ))
 
   (testing "Select all links like this"
-    (let [result (links "https://www.google.com")]
+    (let [result (links earl)]
       (is (< 0 (count result)))
       ))
 
+  (testing "Make an earl like this"
+    (let [result (to-earl "http://google.com")]
+      (is (= java.net.URL (type result)))
+      ))
+
   (testing "Select all links with css class by filtering like this"
-    (let [result (filter (fn [e] (has-class? e "gb1")) (links "https://www.google.com"))]
-      (println result)
+    (let [result (filter (fn [e] (has-class? e "gb1")) (links earl))]
       (is (< 0 (count result)))
       ))
 
