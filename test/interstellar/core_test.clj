5d6ba4b1c33a8e6c5078a9eccea67191e496a0cc
Ben Biddington <ben.biddington@gmail.com>
2015-02-05 12:25:12 +1300

    Better output formatting

--- test/interstellar/core_test.clj
+++ test/interstellar/core_test.clj
@@ -10,6 +10,9 @@
 (defn top[]
   (title-find 1))
 
+(defn print-list[ratings]
+  (doseq [rating ratings] (println (str (get rating :title) ", score: " (get rating :score) ", metascore: " (get rating :metascore)))))
+
 (deftest end-to-end-examples
   (testing "can find the rating of the first item on the screen and print it here"
     (let [result (top)]
@@ -17,6 +20,6 @@
 
   (testing "can for example, get the top 25 in the list"
     (let [result (title-find 25)]
-      (println result)
+      (print-list result)
       (is (= 25 (count result)))))
       )
