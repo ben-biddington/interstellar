ad5173fda781b482f3c092027380b21d77657d59
Ben Biddington <ben.biddington@gmail.com>
2015-02-04 14:31:39 +1300

    How to read and write hashmaps to and from disk

--- test/interstellar/disk_cache_test.clj
+++ test/interstellar/disk_cache_test.clj
@@ -14,3 +14,19 @@
     (let [new-cache (cache/miss C "A" "value A")]
       (is (= "value A" (cache/lookup new-cache "A")))))
   )
+
+(defn save-map[map filename]
+  (binding [*print-dup* true]
+    (spit filename map)))
+
+(defn read-map[filename]
+  (binding [*print-dup* true]
+    (read-string(slurp filename))))
+
+(deftest how-to-write-a-hashmap-to-a-file-on-disk
+  (testing "Can write a has to disk and read it back"
+    (let [filename ".tmp"]
+      (save-map (hash-map :a :value_a, :b :value_b) filename)
+      (let [loaded-map (read-map filename)]
+        (is (= :value_a (get loaded-map :a)))))))
+
