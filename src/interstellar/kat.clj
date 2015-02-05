644107c9eafa711f0eb027a75f37fede072e8624
Ben Biddington <ben.biddington@gmail.com>
2015-02-05 13:26:56 +1300

    Formatting

--- src/interstellar/kat.clj
+++ src/interstellar/kat.clj
@@ -6,42 +6,34 @@
   (:require [net.cgrand.tagsoup :as tagsoup])
   (:require [net.cgrand.xml :as xml]))
 
+(def ^{:private true} earl "http://kickass.so")
+(def ^{:private true} movies-earl (str earl "/movies/"))
+
 (defn ^{:private true} gzip-html-parser [stream]
   (with-open [^java.io.Closeable stream stream]
     (let [zip (GZIPInputStream. stream)]
     (tagsoup/parser zip))))
 
-(defn ^{:private true} to-earl[text]
- (URL. text))
+(defn ^{:private true} to-earl[text] (URL. text))
 
 (defn ^{:private true} browser-get[earl,selector]
   (select (html-resource (to-earl earl) {:parser gzip-html-parser }) [selector]))
 
-(defn ^{:private true} body[earl]
-  (browser-get earl :body))
-
-(defn ^{:private true} links[earl]
-  (browser-get earl :a))
+(defn ^{:private true} body[earl] (browser-get earl :body))
+(defn ^{:private true} links[earl] (browser-get earl :a))
 
 (defn ^{:private true} has-href-matching?[element, name]
   (let [href (get (get element :attrs) :href)]
     (if (nil? href) nil (= true (.contains href name)))))
 
-(defn ^{:private true} has-class?[element, name]
-  (= name (get (get element :attrs) :class)))
-
-(def ^{:private true} earl "http://kickass.so")
-(def ^{:private true} movies-earl (str earl "/movies/"))
-
-(defn ^{:private true} href[link]
-  (get (get link :attrs) :href))
+(defn ^{:private true} has-class?[element, name] (= name (get (get element :attrs) :class)))
+(defn ^{:private true} href[link] (get (get link :attrs) :href))
 
 (defn ^{:private true} detail-earls[]
   (map (fn[link] (href link))
     (filter (fn[link] (has-class? link "cellMainLink")) (links movies-earl))))
 
-(defn ^{:private true} detail[name]
-  (links (str earl name)))
+(defn ^{:private true} detail[name] (links (str earl name)))
 
 (defn imdb-link[url]
   (let [link (filter (fn [e] (has-href-matching? e "imdb")) (detail url))]
