dbf123974f9f96bddd6f00de326e59e1e0fc9b45
Ben Biddington <ben.biddington@gmail.com>
2015-02-05 23:14:18 +1300

    Formatting

--- test/interstellar/rss_feed_test.clj
+++ test/interstellar/rss_feed_test.clj
@@ -1,12 +1,10 @@
 (ns interstellar.rss-feed-test
   (:require [clojure.test :refer :all]
 	    [clojure.java.io :as io]
-            [clojure.xml :as xml]
-	    ))
+            [clojure.xml :as xml]))
 
 (defn rss[earl]
   (xml/parse earl))
-
 
 ;; https://github.com/clojure-cookbook/clojure-cookbook/blob/master/04_local-io/4-22_read-write-xml.asciidoc
 ;; https://clojuredocs.org/clojure.core/for
@@ -15,8 +13,7 @@
   (testing "can read rss"
     (let [rss-feed (rss "http://kickass.so/movies/2/?rss=true")]
       (let [links (for [n (xml-seq rss-feed) :when (= :link (:tag n))] (:content n))]
-        (println "Thesea are the links, the first one needs to go though: ")
-        (println links)
-        )))
+        (println "These are the links, the first one needs to go though: ")
+        (println links))))
   )
 
