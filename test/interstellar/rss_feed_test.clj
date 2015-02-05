6b30b14c13515a595238b174402d7528e1907697
Ben Biddington <ben.biddington@gmail.com>
2015-02-05 23:12:10 +1300

    Added links

--- test/interstellar/rss_feed_test.clj
+++ test/interstellar/rss_feed_test.clj
@@ -8,13 +8,15 @@
   (xml/parse earl))
 
 
-
+;; https://github.com/clojure-cookbook/clojure-cookbook/blob/master/04_local-io/4-22_read-write-xml.asciidoc
+;; https://clojuredocs.org/clojure.core/for
+;; http://gettingclojure.wikidot.com/cookbook:xml-html
 (deftest finding-imdb-results
   (testing "can read rss"
     (let [rss-feed (rss "http://kickass.so/movies/2/?rss=true")]
       (let [links (for [n (xml-seq rss-feed) :when (= :link (:tag n))] (:content n))]
         (println "Thesea are the links, the first one needs to go though: ")
-(println links)
+        (println links)
         )))
   )
 
