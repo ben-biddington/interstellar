7bbe0d13156def065a0958e2cfd78d56daa2cf92
Ben Biddington <ben.biddington@gmail.com>
2015-02-06 07:35:39 +1300

    Refactor: extracted function

--- test/interstellar/rss_feed_test.clj
+++ test/interstellar/rss_feed_test.clj
@@ -6,14 +6,14 @@
 (defn rss[earl]
   (xml/parse earl))
 
+(defn rss-links[earl]
+  (let [rss-feed (rss earl)]
+     (for [n (xml-seq rss-feed) :when (= :link (:tag n))] (:content n))))
+
 ;; https://github.com/clojure-cookbook/clojure-cookbook/blob/master/04_local-io/4-22_read-write-xml.asciidoc
 ;; https://clojuredocs.org/clojure.core/for
 ;; http://gettingclojure.wikidot.com/cookbook:xml-html
 (deftest finding-imdb-results
-  (testing "can read rss"
-    (let [rss-feed (rss "http://kickass.so/movies/2/?rss=true")]
-      (let [links (for [n (xml-seq rss-feed) :when (= :link (:tag n))] (:content n))]
-        (println "These are the links, the first one needs to go though: ")
-        (println links))))
-  )
+  (testing "can read all the links on an rss screen, omitting the first (because it is outside an item node)"
+        (println (drop 1 (rss-links "http://kickass.so/movies/1/?rss=true&field=seeders&sorder=desc")))))
 
