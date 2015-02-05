cf2817f0c918ada454a349cc906e157efea3a920
Ben Biddington <ben.biddington@gmail.com>
2015-02-06 08:06:09 +1300

    Can find the first n pages of link text (detail earls)

--- test/interstellar/rss_feed_test.clj
+++ test/interstellar/rss_feed_test.clj
@@ -8,20 +8,25 @@
 
 (defn rss-links[earl]
   (let [rss-feed (rss earl)]
-     (for [n (xml-seq rss-feed) :when (= :link (:tag n))] (:content n))))
+     (for [n (xml-seq rss-feed) :when (= :link (:tag n))] (first (:content n)))))
 
-(defn kat-rss-links[n]
+(defn kat-rss-links-page[n]
   (drop 1 (rss-links (str "http://kickass.so/movies/" n "/?rss=true&field=seeders&sorder=desc"))))
+
+(defn kat-rss-links
+  "Find n pages of rss links"
+  [n]
+  (flatten (pmap kat-rss-links-page (range 1 4))))
 
 ;; https://github.com/clojure-cookbook/clojure-cookbook/blob/master/04_local-io/4-22_read-write-xml.asciidoc
 ;; https://clojuredocs.org/clojure.core/for
 ;; http://gettingclojure.wikidot.com/cookbook:xml-html
 (deftest finding-imdb-results
   (testing "can read all the links on an rss screen, omitting the first (because it is outside an item node)"
-    (println (drop 1 (rss-links "http://kickass.so/movies/1/?rss=true&field=seeders&sorder=desc"))))
+    (is (= 25 (count (drop 1 (rss-links "http://kickass.so/movies/1/?rss=true&field=seeders&sorder=desc"))))))
 
-  (testing "can find say 10 pages at once"
-    
-    (println (drop 1 (rss-links "http://kickass.so/movies/1/?rss=true&field=seeders&sorder=desc"))))  
-  )
+  (testing "can find say the first 3 pages at once"
+    (let [result (kat-rss-links 3)]
+      (is (= 75 (count result)))))
+)
 
