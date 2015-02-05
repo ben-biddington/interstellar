3f4a8c717f4621c259cb4be9ba5fda39117a0623
Ben Biddington <ben.biddington@gmail.com>
2015-02-06 07:40:51 +1300

    Refactor: extracted function

--- test/interstellar/rss_feed_test.clj
+++ test/interstellar/rss_feed_test.clj
@@ -10,10 +10,18 @@
   (let [rss-feed (rss earl)]
      (for [n (xml-seq rss-feed) :when (= :link (:tag n))] (:content n))))
 
+(defn kat-rss-links[n]
+  (drop 1 (rss-links (str "http://kickass.so/movies/" n "/?rss=true&field=seeders&sorder=desc"))))
+
 ;; https://github.com/clojure-cookbook/clojure-cookbook/blob/master/04_local-io/4-22_read-write-xml.asciidoc
 ;; https://clojuredocs.org/clojure.core/for
 ;; http://gettingclojure.wikidot.com/cookbook:xml-html
 (deftest finding-imdb-results
   (testing "can read all the links on an rss screen, omitting the first (because it is outside an item node)"
-        (println (drop 1 (rss-links "http://kickass.so/movies/1/?rss=true&field=seeders&sorder=desc")))))
+    (println (drop 1 (rss-links "http://kickass.so/movies/1/?rss=true&field=seeders&sorder=desc"))))
+
+  (testing "can find say 10 pages at once"
+    
+    (println (drop 1 (rss-links "http://kickass.so/movies/1/?rss=true&field=seeders&sorder=desc"))))  
+  )
 
