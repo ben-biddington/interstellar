091e0b9910949febfe8b2d24e09326502e702ba1
Ben Biddington <ben.biddington@gmail.com>
2015-02-10 13:13:48 +1300

    Change an atom's value using swap!

--- test/interstellar/atoms_test.clj
+++ test/interstellar/atoms_test.clj
@@ -6,8 +6,19 @@
 (def an-atom
   (atom {:count 1}))
 
+;; See: <http://www.braveclojure.com/concurrency/>
 (deftest things-that-vary-over-time
-  (testing "changing value over time"
+  (testing "an atom is an atom"
+    (is (= clojure.lang.Atom (type an-atom))))
+
+  (testing "query its value like this"
     (is (= 1 (:count @an-atom))))
+
+  (testing "change its value like this"
+    (swap! an-atom 
+      (fn[current-state] 
+        (merge-with + current-state {:count 2})))
+
+    (is (= 3 (:count @an-atom))))
   )
 
