02922a84828bede17a8748d143b53ac9748814f8
Ben Biddington <ben.biddington@gmail.com>
2015-02-12 10:51:34 +1300

    score and metascore are numbers

--- test/interstellar/imdb_test.clj
+++ test/interstellar/imdb_test.clj
@@ -8,16 +8,16 @@
 (deftest finding-imdb-results
   (testing "can, for example, find robocop by name"
     (let [result (imdb-find "robocop")]
-      (is (= "67"        (get result :metascore)))
-      (is (= "7.5"       (get result :score)))
+      (is (= 67        (get result :metascore)))
+      (is (= 7.5       (get result :score)))
       (is (= "RoboCop" (get result :title)))))
 
   (testing "can find by imdb id"
     (let [result (imdb-find-by-id "tt0076759")]
       (is (= "Star Wars: Episode IV - A New Hope" (get result :title))
-      (is (= "8.7"                                  (get result :score))))))
+      (is (= 8.7                                  (get result :score))))))
 
-  (testing "can find by more than one at a time"
+  (testing "can find by more than one at a time (also uses theading operatior: ->)"
     (let [result (first (imdb-find-multi-by-id "tt0076759" "tt0120907"))]
-      (is (= "Star Wars: Episode IV - A New Hope" (get result :title))
-      (is (= "8.7"                                  (get result :score)))))))
+      (is (= "Star Wars: Episode IV - A New Hope" (-> :title result))
+      (is (= 8.7                                  (-> :score result)))))))
