3053dd404a1963b1f8ae316d1fd5b7b72904a212
Ben Biddington <ben.biddington@gmail.com>
2015-04-13 21:47:37 +1200

    [CACHING] Added `nice-browser-get`

--- src/interstellar/cache.clj
+++ src/interstellar/cache.clj
@@ -1,7 +1,13 @@
-(ns interstellar.cache)
+(ns interstellar.cache
+  (:refer-clojure :exclude [get set]))
   
 (def -cache (atom {}))
 
 (def clear (reset! -cache {}))
 
-(defn has?[key] (contains? @-cache key))
+(defn- has?[key] (contains? @-cache key))
+
+(defn get[key] (clojure.core/get @-cache key))
+
+(defn set[key value] 
+  (swap! -cache #(conj {key value} %)))
--- test/interstellar/internet_test.clj
+++ test/interstellar/internet_test.clj
@@ -1,15 +1,16 @@
 (ns interstellar.internet-test
+  (:refer-clojure :exclude [get set])
   (:require [clojure.test :refer :all]
             [interstellar.t-internet :refer :all :as net]
             [interstellar.cache :refer :all :as cache]))
 
-(defn- browser-get[earl, selector]
-  (net/get-gzip (get-gzip earl) [selector]))
-
-(defn- nice-browser-get[f cache]
+(defn- nice-browser-get[f]
   (fn[url]
-    (f url)
-    ))
+    (dosync
+     (let [cached (cache/get url)]
+       (when (nil? cached)
+         (cache/set url (apply f [url])))
+       (cache/get url)))))
 
 (deftest that-you-can-fetch-a-resource
   (testing "that it returns a data structure representing the page"
@@ -19,6 +20,8 @@
 (deftest that-you-can-cache-replies
   (testing "for example, requesting the same URL twice only produces one request"
     (net/zero)
-    (net/get-gzip "http://kickass.to/movies")
-    (net/get-gzip "http://kickass.to/movies")
-    (is (= 1 (net/request-count)))))
+    (let [nice-get (nice-browser-get #(net/get-gzip %))]
+      (dotimes [n 2]
+        (apply nice-get ["http://kickass.to/movies"])))
+    
+    (is (= 1 (net/request-count)) "Expected exactly one request because the result ought to have been cached" )))
