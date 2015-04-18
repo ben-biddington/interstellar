f6fc46b267bdac08e5612c569b81e1fcf93fa6c4
Ben Biddington <ben.biddington@gmail.com>
2015-04-19 10:40:40 +1200

    Yes, you _can_ cache vectors on disk

--- test/interstellar/disk_cache_test.clj
+++ test/interstellar/disk_cache_test.clj
@@ -6,14 +6,22 @@
 (def cache-file ".tmp")
 
 (defn before[fn]
-  (disk-cache/clear cache-file))
+  (disk-cache/clear cache-file)
+  (apply fn []))
 
 (use-fixtures :each before)
 
-(deftest how-to-write-a-hashmap-to-a-file-on-disk
-  (testing "Can write a hash to disk and read it back"
+(deftest can-write-a-hashmap-to-a-file
+  (testing "for example"
     (let [filename cache-file]
       (disk-cache/save (hash-map :a :value_a, :b :value_b) cache-file)
       (let [loaded-map (disk-cache/read cache-file)]
         (is (= :value_a (get loaded-map :a)))))))
+
+(deftest can-write-vector-to-file
+  (testing "for example"
+    (let [filename cache-file]
+      (disk-cache/save ["Star Wars I" "Star Wars II", "Star Wars III"] cache-file)
+      (let [loaded-vector (disk-cache/read cache-file)]
+        (is (some #{"Star Wars I"} loaded-vector))))))
 
