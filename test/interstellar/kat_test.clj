e4584ca4b54810236eb2e1a6241f8a7fc20e73cc
Ben Biddington <ben.biddington@gmail.com>
2015-04-24 20:59:03 +1200

    Deleted unused imports

--- test/interstellar/kat_test.clj
+++ test/interstellar/kat_test.clj
@@ -1,10 +1,4 @@
 (ns interstellar.kat-test
-  (:use net.cgrand.enlive-html)
-  (:import java.net.URL) 
-  (:import java.lang.String)
-  (:import java.util.zip.GZIPInputStream)
-  (:require [net.cgrand.tagsoup :as tagsoup])
-  (:require [net.cgrand.xml :as xml])
   (:require [clojure.test :refer :all]
             [interstellar.kat :refer :all]))
 
